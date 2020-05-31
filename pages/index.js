86623882e5a476f05c81335b30eb4b1b1c34a9c2
Ardee <ardee.aram@gmail.com>
2020-05-31 13:48:18 +0000

    Example GraphQL Admin API Query

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,80 @@
-import { Heading, Page, Button, Card, DataTable } from "@shopify/polaris";
-import { Query } from 'react-apollo';
-import { useLazyQuery } from '@apollo/react-hooks';
+import {
+  Heading,
+  Page,
+  Button,
+  Card,
+  DataTable,
+  Layout,
+} from "@shopify/polaris";
+import { Query } from "react-apollo";
+import { useLazyQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
+import { useState } from "react";
 
 const CHECK_SHOP = gql`
-query {
-  shop {
-    name
-    primaryDomain {
-      host
+  query {
+    shop {
+      name
+      primaryDomain {
+        host
+      }
+      myshopifyDomain
+      plan {
+        displayName
+      }
+      timezoneAbbreviation
     }
-    myshopifyDomain
-    plan {
-      displayName
-    }
-    timezoneAbbreviation
   }
-}`;
+`;
 
 const Index = () => {
-  const [fetchStoreDetails, { loading, error, data }] = useLazyQuery(CHECK_SHOP);
-  
+  const [fetchStoreDetails, { loading, error, data }] = useLazyQuery(
+    CHECK_SHOP
+  );
+  const [show, setShow] = useState(false);
+
+  const clickMe = () => {
+    if (!show) {
+      fetchStoreDetails();
+    }
+    setShow((x) => !x);
+  };
 
   return (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-    
-    <Button size="large" primary={true} onClick={fetchStoreDetails()}>Fetch Store Details.</Button>
+    <Page>
+      <Layout>
+        <Layout.Section>
+          <Card sectioned>
+            <Heading>Shopify app with Node and React 🎉</Heading>
+            <Button size="large" primary={true} onClick={() => clickMe()}>
+              Graphql Admin API Query Example.
+            </Button>
+          </Card>
+          {show && (
+            <Card title="Graphql Admin API Query Example" sectioned>
+              <div>
+                {loading && <div>Loading...</div>}
+                {error && <div>Error :(</div>}
 
-    <div style={{marginTop: "15px"}}>
-      <Card title="Online Store Details" sectioned >
-        <div>
-          
-              {loading && (<div>Loading...</div>)}
-              {error && (<div>Error :(</div>)}
-
-              {data && data.shop && (
-
-                <DataTable
-                  columnContentTypes={[
-                    'text',
-                    'text',
-                  ]}
-                  headings={[
-                    'Field',
-                    'Value',
-                  ]}
-                  rows={[
-                    ["Name", data.shop.name],
-                    ["Domain", data.shop.primaryDomain.host],
-                    ["MyShopify Domain", data.shop.myshopifyDomain],
-                    ["Plan", data.shop.plan.displayName],
-                    ["Timezone", data.shop.timezoneAbbreviation],
-                  ]}
-                />
-
-              )}
-          </div>
-      </Card>
-    </div>
-  </Page>
+                {data && data.shop && (
+                  <DataTable
+                    columnContentTypes={["text", "text"]}
+                    headings={["Field", "Value"]}
+                    rows={[
+                      ["Name", data.shop.name],
+                      ["Domain", data.shop.primaryDomain.host],
+                      ["MyShopify Domain", data.shop.myshopifyDomain],
+                      ["Plan", data.shop.plan.displayName],
+                      ["Timezone", data.shop.timezoneAbbreviation],
+                    ]}
+                  />
+                )}
+              </div>
+            </Card>
+          )}
+        </Layout.Section>
+      </Layout>
+    </Page>
   );
 };
 
